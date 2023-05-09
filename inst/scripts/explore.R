a66527d8d3627f84376d789683feaeddaff4e3f6
Samuel Unicomb <samuelunicomb@gmail.com>
2023-05-09 13:59:14 +0100

    fix typo in inst/scripts

--- inst/scripts/explore.R
+++ inst/scripts/explore.R
@@ -3,10 +3,9 @@ library("ggplot2")
 
 S <- surveygraphr::gensurvey(200, 25)
 
-results <- surveygraphr::exploregraphs(S)
+results <- surveygraphr::exploregraph(S)
 
 df <- data.frame(radius = unlist(results[1]), degree = unlist(results[2]), LCC = unlist(results[3]))
 
-ggplot(df, aes(x = radius, y = LCC)) + labs(title="size of the largest connected component as a function of radius") + geom_point()
-
-ggplot(df, aes(x = radius, y = z)) + labs(title="average degree as a function of radius") + geom_point()
+ggplot(df, aes(x = radius, y = LCC)) + geom_point()
+ggplot(df, aes(x = radius, y = z)) + geom_point()
--- inst/scripts/graph.R
+++ inst/scripts/graph.R
@@ -6,5 +6,4 @@ S <- surveygraphr::gensurvey(200, 25)
 edgelist <- surveygraphr::listgraph(S)
 
 g <- make_graph(edges = edgelist, directed = FALSE)
-
 plot(g, vertex.size=5, vertex.label=NA)
