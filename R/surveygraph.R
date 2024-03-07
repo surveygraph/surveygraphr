b1c12aef1574abbe22ccdeaad3ad6534cf7ca9e3
Samuel Unicomb <samuelunicomb@gmail.com>
2024-03-07 10:03:52 +0000

    update vignettes

--- R/surveygraph.R
+++ R/surveygraph.R
@@ -1,13 +1,15 @@
 #' surveygraph: network representations of attitudes
 #'
-#' The surveygraph package provides the following functions....
+#' This page is a work in progress.
+#'
+#' The surveygraph package functions can be summarised as follows.
 #'
 #' @section reading functions:
 #' The reading functions import survey datasets to R so they can be 
 #' passed to C++ routines. A bunch of file formats need to be accounted for.
 #'
 #' @section network generating functions:
-#' The network functions are implemented in C++, and blah blah blah.
+#' The network functions are implemented in C++.
 #'
 #' @docType package
 #' @name surveygraph
