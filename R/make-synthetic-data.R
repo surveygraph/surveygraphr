726626d837fe555d49de38ae4d6eebfce0275414
Samuel Unicomb <samuelunicomb@gmail.com>
2025-09-22 13:34:53 +0100

    refactor synthetic methods, do not expect r cmd check to pass

--- R/make-synthetic-data.R
+++ R/make-synthetic-data.R
@@ -15,156 +15,187 @@
 #' @param polarisation The degree of polarisation among the system's agents
 #' @param correlation Probability that group item corresponds to polarisation
 #' @param scale Range of the Likert scale
+#' @param seed Seed value for random number generation.
 #' 
 #' @export
 #' @examples
 #' S <- make_synthetic_data(200, 8)
 #' @export
-make_synthetic_data <- function(nrow, 
-                                ncol,
-                                minority = 0.5,
-                                polarisation = 0,
-                                correlation = 0.85,
-                                scale = 10){
+make_synthetic_data <- function(
+  nrow, 
+  ncol,
+  minority = NULL,
+  polarisation = NULL,
+  correlation = NULL,
+  scale = NULL,
+  seed = NULL,
+  ...
+){
+  # TODO:
+  # rename `scale` to `likert`, deprecation warning, use ... argument for this
+  # accept both UK and US spelling, polarisation and polarization. use ... argument for this.
 
-  minority <- clean_minority(minority)
-  polarisation <- clean_polarisation(polarisation)
-  correlation <- clean_correlation(correlation)
-  scale <- clean_scale(scale)
+  # Validate `nrow` argument.
+  if(!is.numeric(nrow))
+    stop("`nrow` argument must be a non-negative integer.", call. = F)
 
-  data <- data.frame(matrix(NA, nrow = nrow, ncol = ncol + 1))
+  if(length(nrow) != 1)
+    stop("`nrow` argument must be of length 1.", call. = F)
 
-  avg <- scale / 2 + (1 - scale / 2) * polarisation
-  avgflag <- 0
+  if(is.na(nrow))
+    stop("`nrow` argument cannot be NA.", call. = F)
 
-  response_hi <- (scale + (scale - 1) * polarisation) / 2
-  response_lo <- (scale - (scale - 1) * polarisation) / 2
+  if(nrow < 0)
+    stop("`nrow` argument must be a non-negative integer.", call. = F)
+
+  if(nrow != as.integer(nrow))
+    stop("`nrow` argument must be a non-negative integer.", call. = F)
+
+  nrow <- as.integer(nrow)
+
+
+  # Validate `ncol` argument.
+  if(!is.numeric(ncol))
+    stop("`ncol` argument must be a non-negative integer.", call. = F)
+
+  if(length(ncol) != 1)
+    stop("`ncol` argument must be of length 1.", call. = F)
+
+  if(is.na(ncol))
+    stop("`ncol` argument cannot be NA.", call. = F)
+
+  if(ncol < 0)
+    stop("`ncol` argument must be a non-negative integer.", call. = F)
+
+  if(ncol != as.integer(ncol))
+    stop("`ncol` argument must be a non-negative integer.", call. = F)
+
+  ncol <- as.integer(ncol)
+
+
+  # Validate `minority` argument.
+  if(is.null(minority))
+    minority <- 0.5
+
+  if(!is.numeric(minority))
+    stop("`minority` argument must be between 0 and 0.5, inclusive.", call. = F)
+
+  if(length(minority) != 1)
+    stop("`minority` argument must be of length 1.", call. = F)
+
+  if(is.na(minority))
+    stop("`minority` argument cannot be NA.", call. = F)
+
+  if(minority < 0 || minority > 1)
+    stop("`minority` argument must be between 0 and 0.5, inclusive.", call. = F)
+
+  if(minority > 0.5){
+    warning("'minority' argument must be between 0 and 0.5 inclusive, taking 1 - minority.")
+    minority <- 1 - minority
+  }
+
+  minority <- as.numeric(minority)
+
+
+  # Validate `polarisation` argument.
+  if(is.null(polarisation))
+    polarisation <- 0
+
+  if(!is.numeric(polarisation))
+    stop("`polarisation` argument must be between 0 and 1, inclusive.", call. = F)
+
+  if(length(polarisation) != 1)
+    stop("`polarisation` argument must be of length 1.", call. = F)
+
+  if(is.na(polarisation))
+    stop("`polarisation` argument cannot be NA.", call. = F)
+
+  if(polarisation < 0 || polarisation > 1)
+    stop("`polarisation` argument must be between 0 and 1, inclusive.", call. = F)
+
+  polarisation <- as.numeric(polarisation)
+
+
+  # Validate `correlation` argument.
+  if(is.null(correlation))
+    correlation <- 0.85
+
+  if(!is.numeric(correlation))
+    stop("`correlation` argument must be between 0 and 1, inclusive.", call. = F)
+
+  if(length(correlation) != 1)
+    stop("`correlation` argument must be of length 1.", call. = F)
+
+  if(is.na(correlation))
+    stop("`correlation` argument cannot be NA.", call. = F)
+
+  if(correlation < 0 || correlation > 1)
+    stop("`correlation` argument must be between 0 and 1, inclusive.", call. = F)
+
+  correlation <- as.numeric(correlation)
+
+
+  # Validate `scale` argument.
+  if(is.null(scale))
+    scale <- 10
+
+  if(!is.numeric(scale))
+    stop("`scale` argument must be a positive integer.", call. = F)
+
+  if(length(scale) != 1)
+    stop("`scale` argument must be of length 1.", call. = F)
+
+  if(is.na(scale))
+    stop("`scale` argument cannot be NA.", call. = F)
+
+  if(scale < 0)
+    stop("`scale` argument must be a positive integer.", call. = F)
+
+  if(scale != as.integer(scale))
+    stop("`scale` argument must be a positive integer.", call. = F)
+
+  scale <- as.integer(scale)
+
+
+  # Construct survey.
+  data <- data.frame(matrix(NA, nrow = nrow, ncol = ncol))
+
+  # TODO: if ncol = 1, drop the `group` column.
+  # TODO: remember you're currently allowing zero for nrow and ncol...
+
+  # avgresponse is between 0 and (scale - 1) / 2
+  avgresponse <- (scale - 1) / 2 - ((scale - 1) / 2) * polarisation
 
   for(i in 1:nrow){
-    average_response <- scale / 2
+    # Group membership column, takes a binary value.
+    # TODO: need to fix correlation, corr of 0 needs to be random, not anticorrelated
+    if(runif(1) < correlation)
+      data[i, 1] <- 0
+    else
+      data[i, 1] <- 1
 
-    # group column 
-    if(i < minority * nrow){
-      if(runif(1) < correlation){
-        data[i,1] <- as.character(0)
-      }else{
-        data[i,1] <- as.character(1)
-      }
-    }else{
-      if(runif(1) < correlation){
-        data[i,1] <- as.character(1)
-      }else{
-        data[i,1] <- as.character(0)
-      }
-    }
+    if(i > minority * nrow + 1e-6)
+      data[i, 1] <- 1 - data[i, 1]
 
-    for(j in 1:ncol){
-      if(i < minority * nrow){
-        if(j < 0.5 * ncol){
-          avgflag <- as.integer(1)
-        }else{
-          avgflag <- as.integer(0)
-        }
-      }else{
-        if(j < 0.5 * ncol){
-          avgflag <- as.integer(0)
-        }else{
-          avgflag <- as.integer(1)
-        }
-      }
-
-      data[i,j+1] <- as.numeric(rpois(1, avg))
-      while(data[i,j+1] < 1 | data[i,j+1] > scale){
-        data[i,j+1] <- as.numeric(rpois(1, avg))
-      }
-
-      if(avgflag == 1){
-        data[i,j+1] <- scale + 1 - data[i,j+1]
+    # Likert columns, take on values between 1 and `scale`.
+    for(j in 2:ncol){
+      repeat{
+        data[i, j] <- 1 + as.numeric(rpois(1, avgresponse))
+        if(data[i, j] >= 1 && data[i, j] <= scale)
+          break
       }
       
-      # TEMPORARY allow a fraction of NA values
-      #if(runif(1) < 0.15){
-      #  data[i,j+1] <- NA
-      #}
+      if(i > minority * nrow + 1e-6)
+        data[i, j] <- scale - data[i, j] + 1
     }
   }
 
-  # colnames "group" "item_1" "item_2" ... 
-  n <- c("group")
-  for(i in 1:ncol){
-    dummy <- paste(c("item_", i), collapse="")
-    n <- append(n, dummy) 
-  }
-  colnames(data) <- n
+  # colnames "group" "item_1" "item_2" ... "item_ncol"
+  cnames <- c("group")
+  for(i in 1:(ncol - 1))
+    cnames <- append(cnames, paste(c("item_", i), collapse="")) 
+
+  colnames(data) <- cnames
 
   return(data)
 }
-
-clean_minority <- function(m){
-  if(m < 0.0 | m > 1.0){
-    warning("'minority' must be a fraction between 0 and 0.5, setting to 0.5")
-    m <- 0.5
-  }else if(m > 0.5){
-    warning("'minority' must be between 0 and 0.5, taking 1 - minority")
-    m <- 1 - m
-  }
-  return(m)
-}
-
-clean_polarisation <- function(p){
-  if(p < 0){
-    warning("'polarisation' has been rounded up to 0")
-    p <- 0
-  }else if(p > 1){
-    warning("'polarisation' has been rounded down to 1")
-    p <- 1
-  }
-  return(p)
-}
-
-clean_correlation <- function(c){
-  if(c < 0){
-    warning("'correlation' has been rounded up to 0")
-    c <- 0
-  }else if(c > 1){
-    warning("'correlation' has been rounded down to 1")
-    c <- 1
-  }
-  return(c)
-}
-
-clean_scale <- function(s){
-  if(s < 2){
-    warning("'scale' must be integer and greater than two, setting to 5")
-    s <- 5
-  }
-  return(s)
-}
-
-#  for(i in 1:nrow){
-#    average_response <- scale / 2
-#
-#    if(i < minority * nrow){
-#      if(runif(1) < correlation){
-#        groupinfo[i] <- 0
-#      }else{
-#        groupinfo[i] <- 1
-#      }
-#      average_response <- response_hi
-#    }else{
-#      if(runif(1) < correlation){
-#        groupinfo[i] <- 1
-#      }else{
-#        groupinfo[i] <- 0
-#      }
-#      average_response <- response_lo
-#    }
-#
-#    data[i,1] <- as.character(groupinfo[i])
-#    for(j in 2:(ncol+1)){
-#      data[i,j] <- as.numeric(rpois(1, average_response))
-#      while(data[i,j] < 1 | data[i,j] > scale){
-#        data[i,j] <- as.numeric(rpois(1, average_response))
-#      }
-#    }
-#  }
